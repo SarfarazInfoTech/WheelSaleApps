ff9783e000d881be1d6e9b894d46123add5bd463
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2022-10-14 18:55:40 +0530

    Vehicle Update

--- screens/dealers/VehiclesDetails.js
+++ screens/dealers/VehiclesDetails.js
@@ -1,8 +1,17 @@
-import {View, Text, StyleSheet, Image, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import React, {useEffect, useState} from 'react';
-import {Button, FAB} from 'react-native-paper';
+import {Button, FAB, RadioButton} from 'react-native-paper';
 import {ShownMyVehical} from '../services/UrlApi.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -10,6 +19,8 @@ const VehiclesDetails = ({navigation, route}) => {
   const [dealerId, setDealerId] = useState('');
   const [Price, setPrice] = useState('');
   const [Condition, setCondition] = useState('');
+  const [checked, setChecked] = useState();
+
   const {
     subCategoryId,
     categoryName,
@@ -36,7 +47,7 @@ const VehiclesDetails = ({navigation, route}) => {
     }
   };
 
-  const UpdateVehical = () => {
+  const UpdatePrice = () => {
     const requestOptions = {
       method: 'PUT',
       headers: {
@@ -45,12 +56,11 @@ const VehiclesDetails = ({navigation, route}) => {
       },
       body: JSON.stringify({
         sellingPrice: Price,
-        vehicleCondition: Condition,
       }),
     };
 
     fetch(
-      `http://wheelsale.in:80/wheelsale-app-ws/sub-categories/${subCategoryId}/update`,
+      `http://wheelsale.in:80/wheelsale-app-ws/sub-categories/${subCategoryId}/price`,
       requestOptions,
     )
       .then(response => response.json())
@@ -64,142 +74,351 @@ const VehiclesDetails = ({navigation, route}) => {
       });
   };
 
-  const SoldVehical = () => {
-    fetch(`http://wheelsale.in:80/wheelsale-app-ws/sub-categories/${subCategoryId}/sold`, {
+  const UpdateCondition = () => {
+    const requestOptions = {
       method: 'PUT',
       headers: {
         'content-type': 'application/json',
         accept: 'application/json',
       },
       body: JSON.stringify({
-        sellingPrice: 9999,
-        // vehicleCondition: Condition,
+        vehicleCondition: checked,
       }),
-    }).then(res => res.json()).then(json => {
-      console.log(json.message)
-      // alert("Your Vehical is Sold.")
-      alert(json.message)
-    }).catch( err => {
-      console.log(err)
-    }
+    };
+
+    fetch(
+      `http://wheelsale.in:80/wheelsale-app-ws/sub-categories/${subCategoryId}/condition`,
+      requestOptions,
     )
-  }
+      .then(response => response.json())
+      .then(json => {
+        alert(json.message);
+        console.log(json);
+        setCondition('');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const SoldVehical = () => {
+    fetch(
+      `http://wheelsale.in:80/wheelsale-app-ws/sub-categories/${subCategoryId}/sold`,
+      {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+        },
+        body: JSON.stringify({
+          sellingPrice: 9999,
+        }),
+      },
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.message);
+        alert(json.message);
+        navigation.navigate('Sold Vehicle');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const DeleteVehical = () => {
+    fetch(
+      `http://wheelsale.in:80/wheelsale-app-ws/sub-categories/${subCategoryId}/delete`,
+      {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+        },
+        body: JSON.stringify({
+          sellingPrice: 9999,
+        }),
+      },
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.message);
+        alert(json.message);
+        navigation.navigate('My Vehicle');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     getData();
   });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageBox}>
-        <View style={styles.image}>
-          <Image
-            source={{
-              uri: images,
-            }}
-            style={styles.image}
-          />
-        </View>
-      </View>
-      <View style={styles.hedBox}>
-        <Text style={styles.title}>
-          {company} {categoryName} {subCategoryName} ({modelYear})
-        </Text>
-        <Text style={{fontSize: 14, textTransform: 'capitalize'}}>
-          {color} Color & {vehicleCondition} Condition
-        </Text>
-
-        <View style={styles.flex}>
-          <Text
-            style={{color: 'darkblue', fontSize: 16, alignSelf: 'flex-end'}}>
-            Vehicle Number
-          </Text>
-
-          <View style={styles.vehNo}>
-            <Text
-              style={{
-                color: 'white',
-                textTransform: 'uppercase',
-                paddingLeft: 3,
-              }}>
-              {vehicleNumber}
-            </Text>
+    <>
+      <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
+        <View style={styles.container}>
+          <View style={styles.imageBox}>
+            <View style={styles.image}>
+              <Image
+                source={{
+                  uri: images,
+                }}
+                style={styles.image}
+              />
+            </View>
           </View>
-        </View>
-
-        <View style={styles.flex}>
-          <Text style={styles.price}>Price</Text>
-          <Text style={styles.price}>
-            <FontAwesome name="rupee" size={18} color="#3d3d72" />{' '}
-            {sellingPrice}
-            /-
-          </Text>
-        </View>
-      </View>
-
-      <View style={{}}>
-        {sellingPrice === sellingPrice ? (
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={styles.hedBox}>
+            <Text style={styles.title}>
+              {company} {categoryName} {subCategoryName} ({modelYear})
+            </Text>
             <Text
               style={{
-                fontSize: 18,
+                fontSize: 14,
+                textTransform: 'capitalize',
+                color: 'gray',
               }}>
-              Selling Price :
+              {color} Color & {vehicleCondition} Condition
             </Text>
-            {Price.length != 0 ? (
+
+            <View style={styles.flex}>
+              <Text
+                style={{
+                  color: 'darkblue',
+                  fontSize: 16,
+                  alignSelf: 'flex-end',
+                }}>
+                Vehicle Number
+              </Text>
+
+              <View style={styles.vehNo}>
+                <Text
+                  style={{
+                    color: 'white',
+                    textTransform: 'uppercase',
+                    paddingLeft: 3,
+                  }}>
+                  {vehicleNumber}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.flex}>
+              <Text style={styles.price}>Price</Text>
+              <Text style={styles.price}>
+                <FontAwesome name="rupee" size={18} color="#3d3d72" />{' '}
+                {sellingPrice}
+                /-
+              </Text>
+            </View>
+          </View>
+
+          <View style={{margin: 10}}>
+            {sellingPrice === sellingPrice ? (
+              <View style={{alignItems: 'stretch'}}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: 'black',
+                      marginRight: 10,
+                      alignSelf: 'center',
+                    }}>
+                    Selling Price
+                  </Text>
+                  {Price.length != 0 ? (
+                    <TextInput
+                      value={sellingPrice}
+                      placeholder="Price"
+                      keyboardType="decimal-pad"
+                      onChangeText={Price => setPrice(Price)}
+                      style={{
+                        fontSize: 16,
+                        borderColor: 'lightgray',
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        // paddingHorizontal: 15,
+                        padding: 0,
+                        width: '40%',
+                        textAlign: 'center',
+                        color: 'gray',
+                      }}
+                      placeholderTextColor="gray"
+                    />
+                  ) : (
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        borderColor: 'lightgray',
+                        // borderWidth: 1,
+                        borderRadius: 5,
+                        // paddingHorizontal: 15,
+                        padding: 6,
+                        width: '40%',
+                        alignSelf: 'center',
+                        textAlign: 'center',
+                        color: 'gray',
+                      }}>
+                      {sellingPrice}
+                    </Text>
+                  )}
+
+                  {Price <= 0 ? (
+                    <Button onPress={() => setPrice('Update')}>
+                      <FontAwesome name="pencil" size={18} color="#3d3d72" />
+                    </Button>
+                  ) : null}
+                  {Price.length >= 1 ? (
+                    <Button onPress={() => UpdatePrice()}>UPDATE</Button>
+                  ) : null}
+                </View>
+              </View>
+            ) : Price === 'Update' ? (
               <TextInput
                 value={sellingPrice}
                 placeholder="Selling Price"
                 onChangeText={Price => setPrice(Price)}
-                style={{
-                  fontSize: 18,
-                  borderBottomColor: 'gray',
-                  borderBottomWidth: 1,
-                  marginBottom: 0,
-                  paddingBottom: 0,
-                  alignSelf: 'center',
-                  alignItems: 'center',
-                }}
+                style={styles.textInput}
                 placeholderTextColor="gray"
               />
-            ) : (
-              <Text
-                style={{
-                  fontWeight: '500',
-                  fontSize: 18,
-                  borderBottomColor: 'gray',
-                  borderBottomWidth: 1,
-                }}>
-                {sellingPrice}
-              </Text>
-            )}
-
-            {Price <= 0 ? (
-              <Button onPress={() => setPrice('Update')}>
-                <FontAwesome name="pencil" size={18} color="#3d3d72" />
-              </Button>
-            ) : null}
-            {Price.length >= 1 ? (
-              <Button onPress={() => UpdateVehical()}>SAVE</Button>
             ) : null}
           </View>
-        ) : Price === 'Update' ? (
-          <TextInput
-            value={sellingPrice}
-            placeholder="Selling Price"
-            onChangeText={Price => setPrice(Price)}
-            style={styles.textInput}
-            placeholderTextColor="gray"
-          />
-        ) : null}
-      </View>
-      <Text>Vehicle Condition</Text>
 
-      <Button style={styles.fab} onPress={() => SoldVehical()}>
-        {/* <FontAwesome name="edit" size={18} color="#3d3d72" /> */}
-        <Text style={{color: '#3d3d72'}}>Sold</Text>
-      </Button>
-    </View>
+          <View style={{margin: 10}}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: 'black',
+                  marginRight: 10,
+                  alignSelf: 'center',
+                }}>
+                Vehicle Condition
+              </Text>
+
+              {Condition ? (
+                <>
+                  <View style={styles.radioBtn}>
+                    <RadioButton
+                      value="POOR"
+                      status={checked === 'POOR' ? 'checked' : 'unchecked'}
+                      onPress={() => setChecked('POOR')}
+                      color="red"
+                    />
+                    <Text style={[styles.btnTxt, {color: 'red'}]}>Poor </Text>
+                  </View>
+                  <View style={styles.radioBtn}>
+                    <RadioButton
+                      value="AVERAGE"
+                      status={checked === 'AVERAGE' ? 'checked' : 'unchecked'}
+                      onPress={() => setChecked('AVERAGE')}
+                      color="blue"
+                    />
+                    <Text style={[styles.btnTxt, {color: 'blue'}]}>
+                      Average{' '}
+                    </Text>
+                  </View>
+                  <View style={styles.radioBtn}>
+                    <RadioButton
+                      value="GOOD"
+                      status={checked === 'GOOD' ? 'checked' : 'unchecked'}
+                      onPress={() => setChecked('GOOD')}
+                      color="green"
+                    />
+                    <Text style={[styles.btnTxt, {color: 'green'}]}>Good</Text>
+                  </View>
+                </>
+              ) : (
+                <Text
+                  style={{
+                    fontSize: 16,
+                    borderColor: 'lightgray',
+                    // borderWidth: 1,
+                    borderRadius: 5,
+                    // paddingHorizontal: 15,
+                    padding: 6,
+                    width: '30%',
+                    alignSelf: 'center',
+                    textAlign: 'center',
+                    color: 'gray',
+                  }}>
+                  {vehicleCondition}
+                </Text>
+              )}
+              {Condition <= 0 ? (
+                <Button onPress={() => setCondition('Update')}>
+                  <FontAwesome name="pencil" size={18} color="#3d3d72" />
+                </Button>
+              ) : null}
+              {Condition.length >= 1 ? (
+                <Button onPress={() => UpdateCondition()}>UPDATE</Button>
+              ) : null}
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          position: 'absolute',
+          bottom: 0,
+        }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#f84848',
+            width: '50%',
+            paddingVertical: 10,
+          }}
+          onPress={() =>
+            Alert.alert('Are you sure ', 'You want to delete this vehicle ?', [
+              {
+                text: 'No',
+                onPress: () => console.log('No Pressed'),
+                style: 'No',
+              },
+              {
+                text: 'Delete',
+                onPress: () => DeleteVehical(),
+              },
+            ])
+          }>
+          <Text
+            style={{
+              fontSize: 20,
+              width: '100%',
+              textAlign: 'center',
+              color: 'white',
+            }}>
+            DELETE
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#366666',
+            width: '50%',
+            paddingVertical: 10,
+          }}
+          onPress={() => SoldVehical()}>
+          <Text
+            style={{
+              fontSize: 20,
+              width: '100%',
+              textAlign: 'center',
+              color: 'white',
+            }}>
+            SOLD
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
@@ -216,7 +435,7 @@ const styles = StyleSheet.create({
   imageBox: {},
   card: {},
   hedBox: {
-    padding: 10,
+    padding: 5,
     borderBottomColor: 'lightgray',
     borderBottomWidth: 1,
     margin: 5,
@@ -242,14 +461,14 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     fontWeight: '500',
   },
-  fab: {
-    position: 'absolute',
-    right: 10,
-    bottom: 80,
-    zIndex: 5,
-    borderColor: '#3d3d72',
-    borderWidth: 1,
-    borderRadius: 5,
+  radioBtn: {
+    flexDirection: 'column',
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  btnTxt: {
+    alignSelf: 'center',
+    color: 'black',
   },
 });
 export default VehiclesDetails;
